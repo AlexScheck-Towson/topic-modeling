75eb72f5dbea3a488d629cb14dd502f75c8a56c2
Alex Scheck <aschec2@students.towson.edu>
2020-07-26 14:56:14 -0400

    fixed spelling error and added cybersecurity to stopwords

--- scriptRunner.py
+++ scriptRunner.py
@@ -1,12 +1,14 @@
 import service
 
 stop_words = []
-stop_words.extend(['introduce', 'module', 'introduction', 'issue', 'cover', 'overview', 'topic', 'concept', 'materical', 'focus'])
+stop_words.extend(['introduce', 'module', 'introduction', 'issue', 'cover', 'overview', 'topic', 'concept', 'material', 'focus'])
 stop_words.extend(['student', 'process', 'give', 'basic', 'information', 'include', 'computer', 'operation', 'lesson', 'state'])
 stop_words.extend(['hour', 'base', 'problem', 'build', 'identify', 'presentation', 'common', 'present', 'micromodule', 'discuss'])
 stop_words.extend(['teach', 'class', 'learn', 'exercise', 'lecture', 'activity', 'method', 'important', 'apply', 'step'])
 stop_words.extend(['create', 'explain', 'environment', 'complete', 'hand', 'review', 'nanomodule', 'unit', 'also'])
-stop_words.extend(['technique', 'tool', 'perform', 'principle', 'understand', 'challenge', 'understanding', 'intend', 'topic', 'implement', 'provide', 'application', 'layer'])
+stop_words.extend(['technique', 'tool', 'perform', 'principle', 'understand', 'challenge', 'understanding', 'intend'])
+stop_words.extend(['topic', 'implement', 'provide', 'application', 'layer', 'cybersecurity'])
+
 
 alpha = 30
 num_topics =13
